8c552abc97bd2265f56096d8ff87ba336e714e8d
Thomas Pollinger <thomaspollinger72@gmail.com>
2021-12-05 17:43:18 +1100

    [STB-13]: Unit Testing
    - added jest to front/back and enabled testing core.

---
+++ Core/Utils/__TEST__/dummy.test.ts
@@ -0,0 +1,8 @@
+describe("core test placeholder", () => {
+	it("should pass", () => {
+		expect(true).toBe(true);
+	});
+	it("should fail", () => {
+		expect(true).toBe(false);
+	});
+});
---
+++ Front/src/utils/__TEST__/dummy.test.ts
@@ -0,0 +1,8 @@
+describe("test placeholder", () => {
+	it("should pass", () => {
+		expect(true).toBe(true);
+	});
+	it("should fail", () => {
+		expect(true).toBe(false);
+	});
+});
